7d8c0ade75492fe277c5ea07e1f6d2b69e1991b8
Teri Eyenike <teyenike1@gmail.com>
2021-08-29 21:54:52 +0100

    click up mini assessment project

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,12 @@
-console.log('working on a mini project for clickup!!!');
+const menu = document.querySelector('.navbar__toggle_menu');
+const menuLinks = document.querySelector('.navbar__lists');
+const lis = document.querySelectorAll('.navbar__lists-links');
+
+const toggleMenu = () => {
+  menuLinks.classList.toggle('active');
+  menu.classList.toggle('is-active');
+  lis.forEach((li) => li.classList.toggle('active'));
+};
+
+menu.addEventListener('click', toggleMenu);
+lis.forEach((li) => li.addEventListener('click', toggleMenu));
